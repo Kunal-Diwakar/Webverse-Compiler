d52b7ecaee42f6daffd549310fd9ca4dd3df168c
Kunal Diwakar <kunaldiwakar1999@gmail.com>
2024-08-03 13:58:40 +0530

    Added lazy loading and improved routes structure

---
+++ Client/src/AllRoutes.tsx
@@ -0,0 +1,29 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Loader from "./Shadecn/components/Loader/Loader";
+
+const Home = lazy(() => import("./Pages/Home"));
+const Compiler = lazy(() => import("./Pages/Compiler"));
+const Login = lazy(() => import("./Pages/Login"));
+const Signup = lazy(() => import("./Pages/Signup"));
+const NotFound = lazy(() => import("./Pages/NotFound"));
+
+export default function AllRoutes() {
+  return (
+    <Suspense
+      fallback={
+        <div className="w-full h-[calc(100dvh-60px)] flex justify-center items-center">
+          <Loader />
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compiler/:urlId?" element={<Compiler />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
+  );
+}
--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
 import Header from "./Shadecn/components/Header";
-import Home from "./Pages/Home";
-import Compiler from "./Pages/Compiler";
-import NotFound from "./Pages/NotFound";
 import { ThemeProvider } from "./Shadecn/components/theme-provider";
 import { Toaster } from "./Shadecn/components/ui/sonner";
-import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
 import { useGetUserDetailsQuery } from "./Redux/slices/api";
 import { useDispatch } from "react-redux";
 import { updateCurrentUser, updateIsLoggedIn } from "./Redux/slices/appSlice";
+import AllRoutes from "./AllRoutes";
 function App() {
   const { data, error } = useGetUserDetailsQuery();
   const Diapatch = useDispatch();
@@ -30,14 +25,7 @@ function App() {
       <Toaster position="bottom-right" theme="dark" />
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/compiler" element={<Compiler />} />
-          <Route path="/compiler/:urlId?" element={<Compiler />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AllRoutes />
       </ThemeProvider>
     </>
   );
