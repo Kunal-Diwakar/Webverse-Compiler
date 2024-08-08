b8e28c42ec395d877b08a7878c0018a8c7929f89
Kunal Diwakar <kunaldiwakar1999@gmail.com>
2024-08-09 00:39:57 +0530

    Added dist folder

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,11 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = require("dotenv");
+const dbConnect_1 = require("./lib/dbConnect");
+const userRoutes_1 = require("./routes/userRoutes");
+const compilerRoutes_1 = require("./routes/compilerRoutes");
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.get("/", (req, res) => {
-    return res.status(200).send("ok");
-});
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
+}));
+(0, dotenv_1.config)();
+app.use("/compiler", compilerRoutes_1.compilerRouter);
+app.use("/user", userRoutes_1.userRouter);
+(0, dbConnect_1.dbConnect)();
 app.listen(4000, () => {
     console.log("http://localhost:4000");
 });
