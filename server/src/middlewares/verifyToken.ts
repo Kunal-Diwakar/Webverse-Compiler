b5e676db3517d5504001d921f9aa3bcda3cf6865
Kunal Diwakar <kunaldiwakar1999@gmail.com>
2024-08-09 17:44:01 +0530

    Testing 18

--- server/src/middlewares/verifyToken.ts
+++ server/src/middlewares/verifyToken.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { JsonWebTokenError } from "jsonwebtoken";
+import jwt, { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 
 export interface AuthRequest extends Request {
   _id?: string;
@@ -10,18 +10,26 @@ export const verifyToken = async (
   res: Response,
   next: NextFunction
 ) => {
+  // Extract token from cookies
   const token = req.cookies.token;
 
   if (!token) {
-    return res.status(401).send({ message: "You are unauthorized." });
+    return res.status(401).send({ message: "No token provided. You are unauthorized." });
   }
+
+  // Verify token
   jwt.verify(
     token,
     process.env.JWT_KEY!,
     (err: JsonWebTokenError | null, data: any) => {
       if (err) {
-        return res.status(401).send({ message: "You are unauthorized." });
+        if (err instanceof TokenExpiredError) {
+          return res.status(401).send({ message: "Token expired. Please log in again." });
+        }
+        return res.status(401).send({ message: "Invalid token. You are unauthorized." });
       }
+
+      // Attach user ID to the request object
       req._id = data._id;
       next();
     }
