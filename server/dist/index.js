61e753e1dbbfbb77f4a281f7c6b31f8a2097f7d2
Kunal Diwakar <kunaldiwakar1999@gmail.com>
2024-08-09 14:50:07 +0530

    Testing 11

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -13,14 +15,16 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({
+app.use(
+  (0, cors_1.default)({
     credentials: true,
     origin: ["http://localhost:5173", process.env.CLIENT_URL],
-}));
+  })
+);
 (0, dotenv_1.config)();
 app.use("/compiler", compilerRoutes_1.compilerRouter);
 app.use("/user", userRoutes_1.userRouter);
 (0, dbConnect_1.dbConnect)();
 app.listen(4000, () => {
-    console.log("http://localhost:4000");
+  console.log("http://localhost:4000");
 });
